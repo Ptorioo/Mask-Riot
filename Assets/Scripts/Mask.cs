42d221ca7278f52ce001888361f973337d5032e1
rStar <starlove1247@gmail.com>
2026-02-01 15:21:08 +0800

    面具提示UI

--- Assets/Scripts/Mask.cs
+++ Assets/Scripts/Mask.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Mask : MonoBehaviour
 {
     public Faction faction;
-    
+
     // How close do you need to be? (1.5 units is usually good)
-    [SerializeField] private float pickupRange = 1.5f;
+    [SerializeField]
+    private float pickupRange = 1.5f;
+
+    [SerializeField]
+    private GameObject hint;
+
+    private void Start()
+    {
+        hint.SetActive(false);
+    }
 
     private void Update()
     {
@@ -14,41 +24,41 @@ public class Mask : MonoBehaviour
         if (PlayerHorizontalMovement.Instance == null) return;
 
         // 2. MATH CHECK: Calculate distance directly
-        float distance = Vector2.Distance(transform.position, PlayerHorizontalMovement.Instance.transform.position);
+        float distance = Vector2.Distance(transform.position , PlayerHorizontalMovement.Instance.transform.position);
 
         // 3. Logic: If close enough AND pressing E
-        if (distance <= pickupRange && Keyboard.current.eKey.wasPressedThisFrame)
+        if (distance <= pickupRange)
         {
-            PerformPickup();
+            hint.SetActive(true);
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                PerformPickup();
+            }
+        }
+        else
+        {
+            hint.SetActive(false);
         }
     }
 
-private void PerformPickup()
+    private void PerformPickup()
     {
-        if (faction == Faction.PlayerCharacter) 
+        if (faction == Faction.PlayerCharacter)
         {
             Debug.LogWarning("Mask Faction is Default! Ignoring.");
             return;
         }
 
         // CHANGE: Use GetComponentInChildren to find the sprite even if it's on a sub-object
-        SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
-        
-        if (renderer != null)
-        {
-            PlayerHorizontalMovement.Instance.EquipMask(faction, renderer.sprite);
-            Destroy(gameObject);
-        }
-        else
-        {
-            Debug.LogError($"Mask '{gameObject.name}' has no SpriteRenderer!");
-        }
+        var renderer = GetComponent<SpriteRenderer>();
+        PlayerHorizontalMovement.Instance.EquipMask(faction , renderer.sprite);
+        Destroy(gameObject);
     }
-    
+
     // Optional: Draw a circle in the Editor so you can see the range
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, pickupRange);
+        Gizmos.DrawWireSphere(transform.position , pickupRange);
     }
 }
